ed8589755043094956820fd4a8593eee84b8c4ad
Sahil Rajput <sahilrajput03@gmail.com>
2022-03-08 19:55:00 +0530

    Add binary file to gitignore.

--- learn-fortran/1.f90
+++ learn-fortran/1.f90
@@ -1,4 +1,6 @@
 program hello
   ! This is a comment line; it is ignored by the compiler
   print *, 'Hello, World, India 1234!'
+  print *, 'Hello, World, India 1234!'
+  print *, 'Hello, World, India 1234!'
 end program hello
