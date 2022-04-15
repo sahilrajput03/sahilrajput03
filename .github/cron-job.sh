646581e4958f34aedcd51c436f972f5b29b1e65c
Sahil Rajput <sahilrajput03@gmail.com>
2022-04-15 18:15:20 +0530

    update.

--- .github/cron-job.sh
+++ .github/cron-job.sh
@@ -1,42 +1,38 @@
 #!/usr/bin/env bash
 
 # DEBUGGING LOCALLY via `act` - using ./github/watchCronJob-act.sh script. YO!
-
 # echo ID_RATAN - $ID_RATAN
 # echo ID_SAHIL - $ID_SAHIL
 # echo TOKEN - $TOKEN
+
 while [[ -z $text ]] ; do
-	text=$(shuf ./thoughts-principles.md -n1 | grep '.')
+	text=$(shuf "$(dirname $0)/../thoughts-principles.md" -n1 | grep '.')
 done
 
 echo  text is: $text
 
-echo Sending $text to ratan @ $(date) >> /tmp/ratan-log.txt
+# TODO: Add to secrets
+ID_DHANUR=1765712882
 
+receipients="$ID_RATAN $ID_SAHIL $ID_DHANUR"
+
+# Debugging
 set -x
-curl -X POST \
-	-H "Content-Type: application/json" \
-	-d @<(cat <<EOF 
-{ "chat_id": "$ID_RATAN", "text": "$text", "parse_mode": "HTML" }
+
+for person in ${receipients[@]}; do
+	curl -X POST \
+		-H "Content-Type: application/json" \
+		-d @<(cat <<EOF 
+	{ "chat_id": "$person", "text": "$text", "parse_mode": "HTML" }
 EOF) \
-	"https://api.telegram.org/bot$TOKEN/sendMessage"
+		"https://api.telegram.org/bot$TOKEN/sendMessage"
+done
 
-curl -X POST \
-	-H "Content-Type: application/json" \
-	-d @<(cat <<EOF 
-{ "chat_id": "$ID_SAHIL", "text": "$text", "parse_mode": "HTML" }
-EOF) \
-	"https://api.telegram.org/bot$TOKEN/sendMessage"
-
-
-curl -X POST \
-	-H "Content-Type: application/json" \
-	-d @<(cat <<EOF 
-{ "chat_id": "1765712882", "text": "$text", "parse_mode": "HTML" }
-EOF) \
-	"https://api.telegram.org/bot$TOKEN/sendMessage"
-
-# check event logs of bot:
+# CHECK EVENT LOGS OF BOT:
 # https://api.telegram.org/bot<BOT_TOKEN>/getUpdates
 
-# FYI: This file has to be marked as executable by `chmod +x` else github actions won't be able to run it.
+# MAKING LOGS FOR ACTIVITY:
+# echo Sending $text to ratan @ $(date) >> /tmp/ratan-log.txt
+
+# IMPORTANT:
+# This file has to be marked as executable by `chmod +x` else github actions won't be able to run it.
