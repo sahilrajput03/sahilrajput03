abdbe3a3179f6720577ee6707920ba09a82a348f
Sahil Rajput <sahilrajput03@gmail.com>
2023-12-31 09:30:55 +0530

    Update for correct path for the assets for react build for thoughts and principles build.

--- thoughts-principles-react/vite.config.ts
+++ thoughts-principles-react/vite.config.ts
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // src: https://stackoverflow.com/a/69746868/10012446
+  base: 'thoughts-principles-react/dist', // 👈
 })
