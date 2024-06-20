e960ec73fa5607a2cd65456bfefeb9a635f401de
Sahil Rajput <sahilrajput03@gmail.com>
2024-06-21 03:01:25 +0530

    Update typo.

--- .github/watchBreathingExCron-act.sh
+++ .github/watchBreathingExCron-act.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Note: It is necessary to run to run act.sh file from the root of the repository otherwise act won't work.
+# Note: It is necessary to run `act.sh` file from the root of the repository otherwise act won't work.
 nodemon \
     -x 'act schedule -j=sendBreathingExercisejob --insecure-secrets --secret-file=.secrets-for-act' \
     -w .github/workflows/breathingEx.yaml \
--- .github/watchCronJob-act.sh
+++ .github/watchCronJob-act.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-# Note: It is necessary to run to run act.sh file from the root of the repository otherwise act won't work.
+# Note: It is necessary to run `act.sh` file from the root of the repository otherwise act won't work.
 nodemon \
     -x 'act schedule --insecure-secrets --secret-file=.secrets-for-act' \
     -w .github/workflows/main.yml \
     -w .github/cron-job.sh
+    
