c79810cf6401375768ad82f4d7572ac209cfcc84
Sahil Rajput <sahilrajput03@gmail.com>
2025-02-02 23:48:29 +0530

    Update.

--- searchLinksUtil.js
+++ searchLinksUtil.js
@@ -8,7 +8,7 @@ const textarea = document.createElement("textarea");
 textarea.rows = 1;
 textarea.type = "text";
 textarea.id = "search-input";
-textarea.placeholder = "Search here...";
+textarea.placeholder = "Search here... ⌘K / Ctrl+K";
 textarea.setAttribute("oninput", "searchFunction(event)");
 
 // Add the textarea as the first element
@@ -49,6 +49,15 @@ async function searchFunction(e) {
         .join("");
 
 }
+
+// keyboard shortcut (⌘K / Ctrl+K)
+document.addEventListener('keydown', function (event) {
+    if ((event.ctrlKey || event.metaKey) && event.key === 'k') {
+        event.preventDefault(); // Prevents browser's default behavior
+        document.getElementById('search-input').focus();
+    }
+});
+
 
 // NOTE TO SAHIL: Do not optimize this process to fetch from a separate json file because that cause more issues than the level of simplicity it is supposed to give because json is actually much more delicate which you can break most of times without noticing it because of a comma or adding a comment (because json doesn't support comments in it) or other syntax issues. THUS STICK TO THIS PATTERN ie., to have links in this same file for ease of use.
 
