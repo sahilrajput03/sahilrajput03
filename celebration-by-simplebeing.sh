4d3be005434aee501189e0a51304dfa0705b30ba
Sahil Rajput <sahilrajput03@gmail.com>
2025-05-09 22:04:41 +0530

    Update.

--- celebration-by-simplebeing.sh
+++ celebration-by-simplebeing.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-file="thinking-by-cherry.html"
+file="celebration-by-simplebeing.html"
 wget --output-document=$file "https://docs.google.com/document/d/1kfDwNhR5-BBSyCbtbSrMjM6mr9lczs3DT46o1Y5l9Dg/export?format=html"
 
 # Using prettier for readability of your code: (not necessary but if you use sed to update lines a space after : is added and we need to also specify the space in `sed` as well)
