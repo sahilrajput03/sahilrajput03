efe0158abc1f57bf0cccffb5c9fa70093f0676c1
Sahil Rajput <sahilrajput03@gmail.com>
2025-12-06 18:48:36 +0530

    Update.

--- slideshows/utils.js
+++ slideshows/utils.js
@@ -1,3 +1,4 @@
+// @ts-nocheck
 export function preloadImage(src) {
     return new Promise((resolve, reject) => {
         const img = new Image();
@@ -15,5 +16,30 @@ export function playVideo(video) {
         video.addEventListener("ended", resolve);
         video.addEventListener("error", reject);
         video.play().catch(reject);
+    });
+}
+
+export function addGoToFullscreenButton() {
+    // Using HTML string directly
+    document.body.insertAdjacentHTML("afterbegin", `<div class="goToFullscreenContainer"><button id="goToFullscreen">Go Fullscreen</button></div>`);
+
+    document.getElementById("goToFullscreen").addEventListener("click", async () => {
+        const elem = document.getElementById("slideshow");
+        if (elem.requestFullscreen) {
+            elem.requestFullscreen();
+        } else if (elem.webkitRequestFullscreen) { // Safari
+            elem.webkitRequestFullscreen();
+        } else if (elem.msRequestFullscreen) { // Old IE
+            elem.msRequestFullscreen();
+        }
+
+        // Lock orientation to landscape
+        if (screen.orientation && screen.orientation.lock) {
+            try {
+                await screen.orientation.lock("landscape");
+            } catch (err) {
+                console.warn("Orientation lock failed:", err);
+            }
+        }
     });
 }
