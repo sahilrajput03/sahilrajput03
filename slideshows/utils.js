3faa79920bc44f687d15d7794f14dc87c0a4ff46
Sahil Rajput <sahilrajput03@gmail.com>
2025-12-06 18:37:21 +0530

    Update.

--- slideshows/utils.js
+++ slideshows/utils.js
@@ -6,3 +6,14 @@ export function preloadImage(src) {
         img.onerror = reject;
     });
 }
+
+export const sleep = async (durationMs) => await new Promise((res) => setTimeout(res, durationMs));
+
+// We need this so we return only when video has finished playing.
+export function playVideo(video) {
+    return new Promise((resolve, reject) => {
+        video.addEventListener("ended", resolve);
+        video.addEventListener("error", reject);
+        video.play().catch(reject);
+    });
+}
