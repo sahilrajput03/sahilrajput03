7c1a9d827f613df721becb048d95d374ca88ba2d
Sahil Rajput <sahilrajput03@gmail.com>
2025-12-07 15:25:59 +0530

    Update.

--- slideshows/utils.js
+++ slideshows/utils.js
@@ -102,3 +102,23 @@ export async function hasUserInteracted() {
         return false;
     }
 }
+
+export async function waitSync(checkFunction, interval = 50, timeout = 5_000) {
+    return new Promise((resolve, reject) => {
+        const startTime = Date.now();
+
+        const checkInterval = setInterval(() => {
+            // Check if the condition is met
+            if (checkFunction()) {
+                clearInterval(checkInterval); // Stop checking
+                resolve(); // Resolve the promise
+            }
+
+            // Check for timeout
+            if (Date.now() - startTime > timeout) {
+                clearInterval(checkInterval); // Stop checking
+                reject(new Error('Timeout waiting for condition to be met'));
+            }
+        }, interval);
+    });
+}
